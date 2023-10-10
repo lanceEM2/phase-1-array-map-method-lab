c81e71f30a44df3d54be51d40b8e2e0e7948112c
Dylan <dylanlancekemboi@gmail.com>
2023-10-10 19:01:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalizedWords.join(' ');
+  });
 }
+
+console.log(titleCased());
